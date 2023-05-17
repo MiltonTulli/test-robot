b996ecdd0bf613042107bf571543d1d8edf14f9f
Milton Tulli <milton.tulli@gmail.com>
2023-05-17 14:09:29 -0300

    prisma setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,97 @@
-import Image from 'next/image'
+import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center  p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+          Test
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="mb-12 grid text-center lg:mb-0 lg:grid-cols-12 lg:text-left">
+        <h2 className={`mb-3 text-2xl font-semibold`}>Consignas</h2>
+        <p className={`m-0 max-w-[60ch] text-sm opacity-50`}>
+          El siguiente test va a ser similar al anterior. Va a ser una app de
+          productos con un carrito de compras. Los pasos para completar el test
+          son los siguientes:
+        </p>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="mb-12 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <ul>
+          <li className="mb-3 font-bold ">Páginas:</li>
+          <li className="mb-3 ">/home</li>
+          <li className="mb-3 ">/store</li>
+          <li className="mb-3 ">/product/id</li>
+          <li className="mb-3 ">/contact</li>
+          <li className="mb-3 ">/about</li>
+        </ul>
+      </div>
+      <div className="mb-12 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <h2 className={`mb-3 text-2xl font-semibold`}>Home</h2>
+        <p className={`m-0 max-w-[60ch] text-sm opacity-50`}>
+          Utilizar `src/app/page.txt` para crear una landing de nuestra nueva
+          marca de productos electrónicos llamada &quot;TechZone&quot;
+        </p>
+        <p className={`m-0 max-w-[60ch] text-sm opacity-50`}>Secciones:</p>
+        <p className={`m-0 max-w-[60ch] text-sm opacity-50`}>
+          1. Nav Bar: - Logo de la empresa ( o texto ) - Carrito que despliegue
+          un modal/drawer en caso de tener productos en el carro
+        </p>
+        <p className={`m-0 max-w-[60ch] text-sm opacity-50`}>
+          2. Banner: horizontal con dos columnas. Una para imagen
+          (public/bannner.jpg) y otra para texto.
+        </p>
+        <p className={`m-0 max-w-[60ch] text-sm opacity-50`}>
+          3. Featured products: Mostrar una fila de 4 o 2 productos (dependiendo
+          el tamaño de la pantalla) de los productos destacados. pueden ser solo
+          2 productos random.
+        </p>
+        <p className={`m-0 max-w-[60ch] text-sm opacity-50`}>
+          4. Otro banner de un color llamativo que tenga un texto y un boton
+          hacia la página del store
+        </p>
+        <p className={`m-0 max-w-[60ch] text-sm opacity-50`}>
+          5. Footer con links a página de /about, /contact, /store e información
+          de la empresa (telefono, mail, dirección) y algún texto random tipo
+          slogan
+        </p>
+      </div>
+      <div className="mb-12 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <h2 className={`mb-3 text-2xl font-semibold`}>Store</h2>
+        <p className={`m-0 max-w-[60ch] text-sm opacity-50`}>
+          La página de productos deberá tener:
+          <ul>
+            <li className="mb-3">
+              1. Una sección de filtrado. Debería poder filtrar por precio
+            </li>
+            <li className="mb-3">
+              2. Una sección de buscador, donde busque por nombre del producto
+            </li>
+            <li className="mb-3">
+              3. Listado de los productos encontrados. Si no hay filtro retorno
+              todos productos disponibles. Aplicar paginación
+            </li>
+            <li className="mb-3">
+              4. Cada producto deberá mostrarse en su propia productCard y
+              deberá tener un link para añadirlo al carro o para abrir su página
+              de producto correspondiente (/product/[id])
+            </li>
+          </ul>
+        </p>
+      </div>
+      <div className="mb-12 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <h2 className={`mb-3 text-2xl font-semibold`}>Product page</h2>
+        <p className={`m-0 max-w-[60ch] text-sm opacity-50`}>
+          La página individual de cada producto. Deberá mostrar toda la
+          información disponible del producto y un link para añadir al carro. El
+          carro de momento solo será un context que puede ir agregando o
+          removiendo elementos. Crearlo y conectarlo a cada componente que se
+          vincule al carro
+        </p>
       </div>
     </main>
-  )
+  );
 }
+
+// Usar https://www.firstleaf.club/ de inspiración..
---
+++ src/lib/prisma/index.ts
@@ -0,0 +1,1 @@
+export { getProducts } from "./utils/getProducts";
