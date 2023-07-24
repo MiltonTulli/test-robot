bdd7004fd53ff3cf8396636f40d4f20068898986
Manuel Digregorio <digregorio.mr@gmail.com>
2023-07-24 12:27:38 -0300

    loading state and effect added

--- src/app/products/ProductSearch.tsx
+++ src/app/products/ProductSearch.tsx
@@ -5,8 +5,10 @@ import CardComponent from "@/app/components/CardComponent";
 export default function ProductSearch() {
   const [search, setSearch] = useState("");
   const [filteredProducts, setFilteredProducts] = useState<any[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean >(false)
 
   const onSearch = async () => {
+    setIsLoading(true);
     const url = "http://localhost:3000/api/products";
     try {
       let response = await fetch(`${url}?q=${encodeURIComponent(search)}`);
@@ -16,12 +18,18 @@ export default function ProductSearch() {
     } catch (e) {
       console.log("error");
       setFilteredProducts([]);
+    } finally {
+      setIsLoading(false);
     }
   };
 
+  // por que aca si se puede usar el async await?
+
   useEffect(() => {
     // TODO: cuando carga el componente que haga un fetch para obtener todos los productos.
-  });
+    onSearch();
+    
+  }, []);
 
   return (
     <div className="search-bar">
@@ -33,9 +41,13 @@ export default function ProductSearch() {
         onChange={(e) => setSearch(e.target.value)}
       />
       <button onClick={onSearch}>Search Product</button>
-      <div>-----</div>
+
+      
+
+      <div> {isLoading && <span>Loading...</span>} </div>
 
       {filteredProducts.length === 0 && <span>No products found</span>}
+
       <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
         {filteredProducts.map((product) => (
           <CardComponent
