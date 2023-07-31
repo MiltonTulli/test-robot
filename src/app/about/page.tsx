96a8a39a1665ac9d1941e120d2d648f7584441cf
Manuel Digregorio <digregorio.mr@gmail.com>
2023-07-31 13:43:06 -0300

    about page styling

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -10,7 +10,7 @@ export default function About() {
         <div className="p-4 text-center">
           <h2 className="text-3xl font-semibold text-gray-700 py-2">The latest and
             best tech products</h2> 
-          <h3 className="text-1xl text-gray-700 py-6">Our team of experts
+          <h3 className="text-2xl text-gray-700 p-2 px-8">Our team of experts
            carefully curates the products in our store to ensure they meet the
           highest standards of quality and innovation.
           </h3>
@@ -19,19 +19,19 @@ export default function About() {
         {/* Second Row */}
         <div className="flex flex-col md:flex-row p-4">
           {/* Left Column */}
-          <div className="flex-1 p-4">
-            <p className="text-gray-600 px-4 text-justify">Whether you're a tech enthusiast looking for the latest gadgets or a professional seeking cutting-edge
+          <div className="flex-1 p-4 flex justify-center">
+            <p className="text-gray-600 px-6 text-justify">Whether you're a tech enthusiast looking for the latest gadgets or a professional seeking cutting-edge
               solutions for your work, TechZone is your ultimate tech retail destination. We strive to offer an unbeatable
               selection of products, expert advice, and top-notch customer service to make your shopping experience
               exceptional.</p>
           </div>
 
           {/* Right Column */}
-          <div className="flex-1 p-4">
+          <div className="flex-1 p-4 flex justify-center">
           <Image
             src="/tech.jpeg"
-            width={300}
-            height={300}
+            width={500}
+            height={500}
             alt="Image"
             className="rounded-lg bg-gray-100"
           />
@@ -51,7 +51,7 @@ export default function About() {
 
           {/* Right Column */}
           <div className="flex-1 p-4">
-            <p className="text-gray-600">Join our growing community of tech lovers and stay connected with the latest trends and innovations in the
+            <p className="text-gray-600 px-6 text-justify">Join our growing community of tech lovers and stay connected with the latest trends and innovations in the
     world of technology. We're here to help you embrace the future of tech shopping!</p>
           </div>
         </div>
